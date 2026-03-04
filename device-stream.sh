8be6088fbca305beb2639b71f9a1cce36b420c09
Redwan Ahemed <rashidakhatun2009@gmail.com>
2026-03-04 22:37:22 +0600

    Create device-stream.sh

---
+++ device-stream.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pkg update -y
+pkg upgrade -y
+pkg install x11-repo -y
+pkg update -y
+pkg install tigervnc git python -y
+
+VNC_PASSWORD="xemonbae01"
+
+mkdir -p $HOME/.vnc
+echo $VNC_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
+chmod 600 $HOME/.vnc/passwd
+
+vncserver :1 -geometry 1080x1920 -depth 24 -passwd $HOME/.vnc/passwd
+
+cd $HOME
+if [ ! -d "noVNC" ]; then
+    git clone https://github.com/novnc/noVNC.git
+fi
+cd noVNC
+
+./utils/launch.sh --vnc localhost:5901 --listen 8080
